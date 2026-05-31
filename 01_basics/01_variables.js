650685bcd4a8804c13d2568e5e2b0f2252f62632
Harish Kumar <harishpareek7973658241@gmail.com>
2026-05-31 12:58:33 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12342 
+let accountEmail ="abc@gmail.com" // we will mostly use let keyword
+var accountPassword ="12345"
+accountCity = "Abohar"
+let accountState;
+
+// accountId =2 // this is not allowed 
+accountEmail ="vdccou@gmail.com"
+accountPassword ="65782"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
