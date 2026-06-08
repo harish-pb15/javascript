cdd33c56b3266af12c5551ba3739644cb43dbb57
Harish Kumar <harishpareek7973658241@gmail.com>
2026-06-08 13:00:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat  all JS code as newer version
+
+// alert("hello")  // we are using node JS , not browser. This is used only in browser
+
+// code readability must be maintained
+console.log(9 + 7)
+console.log("abc")
+
+let name = " harish" // string datatype
+let age = 18 // number datatype
+let isLoggedin = true // boolean data type
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value   ---> matlab khali hai ye 
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "abc")  // typeof se hum datatype pta laga skte hai
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
