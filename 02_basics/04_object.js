56bed833ff9ce14bc7a2fd7c5537e516cc641d9a
Harish Kumar <harishpareek7973658241@gmail.com>
2026-06-25 10:01:50 +0000

    object

---
+++ 02_basics/04_object.js
@@ -0,0 +1,19 @@
+// const user = new Object() // singleton object
+const user = {}     // non-singleton object
+console.log(user)
+
+user.id = "123abc"
+user.name = "Harish"
+user.isLoggedin = false
+
+// console.log(user)
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname : {
+        userfullname:{
+            
+        }
+    }
+
+}
