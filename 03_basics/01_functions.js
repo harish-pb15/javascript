1949ab09df704407de37149b57a85cd845e1c683
Harish Kumar <harishpareek7973658241@gmail.com>
2026-07-02 13:31:57 +0000

    functipns

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,11 +15,28 @@ function Myname() {
 
 function addTwoNumbers(number1,number2)
 {
-    let result = number1 + number2
-    return result
+    // let result = number1 + number2
+    // return result
+
+    return number1+number2
     console.log("harish")  // this line will never executed as ye return value k baad likha gya hai
 }
 const result =addTwoNumbers(3,4) // 3 and 4 are called as arguments which are passing value in the function
 
 console.log("Result :", result)
 
+
+
+function loginUserMessage(username)
+{
+    if(username===undefined)  
+        // if(!username){}
+    {
+        console.log("Please,Enter a username")
+        return
+    }
+ return `${username},Just Logged In`
+}
+
+console.log(loginUserMessage("Harish"))
+console.log(loginUserMessage())  //Important, undefined aayega because humne koi argument pass hi nhi kiya function me 
