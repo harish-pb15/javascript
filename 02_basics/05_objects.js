4979f464fe6c16db4335c68f58e39b4afef02279
Harish Kumar <harishpareek7973658241@gmail.com>
2026-07-02 08:51:03 +0000

    destructure object

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,28 @@
+// Object destructuring     Video 18
+
+const course = {
+    coursename: "JS in Hindi ",
+    price: "999",
+    platform: "Youtube"
+}
+
+// course.price
+
+const { platform } = course  // it is as same as const platform =course.platform
+console.log(platform);
+
+
+
+{
+    "name" : "Harish",
+    "coursename" ;" Js in hindi",
+    "price" : "free"
+}
+
+
+[
+    {},
+    {},
+    {}
+
+]
