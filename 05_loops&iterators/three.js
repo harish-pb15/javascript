e41c2bab4d12d07d6b05767abda65f161da47455
Harish Kumar <harishpareek7973658241@gmail.com>
2026-07-04 08:57:30 +0000

    for of loop

---
+++ 05_loops&iterators/three.js
@@ -0,0 +1,38 @@
+// for of 
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) { // of ke baad jispe loop lagana hai usko likhte hai
+    console.log(num)
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(greet)
+}
+
+
+// MAPS
+
+const map = new Map()
+map.set('IN', "India")
+map.set('US', "United States")
+map.set('CN', "Canada")
+map.set('IN', "India") // Map me duplication nhi hota 
+
+console.log(map);
+
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject =
+{
+    'Game1' : 'PUBG',
+    'Game2' : 'FreeFire'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key,value);
+    
+// }
