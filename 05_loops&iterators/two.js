1c10b8a175eb8d9b1ccae2aa505881e85f003c2b
Harish Kumar <harishpareek7973658241@gmail.com>
2026-07-04 08:16:39 +0000

    while and do while loop

---
+++ 05_loops&iterators/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let Myarray = ['flash', "batman", "haau"]
+
+let i = 0;
+while (i < Myarray.length) {
+    console.log(`Value is ${Myarray[i]}`)
+    i++
+
+}
+
+let score = 11
+do {
+    console.log(score)
+    score++
+} while (score <= 10);
